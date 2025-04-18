b03065b96f87657fd7d5f58213901ee309f5be02
huythanh123 <phanhuythanh2509@gmail.com>
2025-04-19 01:31:01 +0700

    add website files

--- js/animation.js
+++ js/animation.js
@@ -11,11 +11,22 @@ if (isFiltersVisible) {
 toggleButton.innerHTML = 'Hide filters <i class="fas fa-sliders-h ps-2"></i>';
 filtersPanel.classList.remove("hidden");
 jobListings.classList.remove("w-full");
-jobListings.classList.add("w-3/4");
+jobListings.classList.add("w-4/4");
 } else {
 toggleButton.innerHTML = 'Show filters <i class="fas fa-sliders-h ps-2"></i>';
 filtersPanel.classList.add("hidden");
-jobListings.classList.remove("w-3/4");
+jobListings.classList.remove("w-4/4");
 jobListings.classList.add("w-full");
 }
+});
+
+function toggleMenu() {
+  const menu = document.getElementById("mobileMenu");
+  menu.classList.toggle("hidden");
+}
+
+// Thêm sự kiện cho nút hamburger
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburgerBtn = document.querySelector("button svg");
+  hamburgerBtn.parentElement.addEventListener("click", toggleMenu);
 });
